984125eb8e969025d4c4902f0351a601dbbe59a1
the one <theone@theoneswebsite.com>
2025-06-28 19:51:39 +0000

    moved project specific code to its own file

--- src/operations.zig
+++ src/operations.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const constants = @import("constants.zig");
+const operations = @import("selection.zig").operations;
 
 pub const Operation = enum(u8) {
     pulse = 0,
@@ -18,38 +19,6 @@ pub fn ResultType(comptime operation: Operation) type {
 pub fn CallType(comptime operation: Operation) fn (EventType(operation), *align(16) [1024]u8) ResultType(operation) {
     return @field(operations, @tagName(operation)).call;
 }
-
-pub const operations = struct {
-    pub const pulse = struct {
-        pub const event = void;
-        pub const result = void;
-        pub fn call(payload: event, output: *align(16) [1024]u8) result {
-            _ = output;
-            _ = payload;
-        }
-    };
-    pub const print = struct {
-        pub const event = void;
-        pub const result = void;
-        pub fn call(payload: event, output: *align(16) [1024]u8) result {
-            _ = output;
-            _ = payload;
-        }
-    };
-    pub const add = struct {
-        pub const event = struct {
-            a: u32,
-            b: u32,
-        };
-        pub const result = u32;
-        pub fn call(payload: event, output: *align(16) [1024]u8) result {
-            _ = output;
-            const added = payload.a + payload.b;
-            std.debug.print("ran add {} + {} = {} \r\n", .{ payload.a, payload.b, added });
-            return added;
-        }
-    };
-};
 
 comptime {
     for (std.meta.fields(operations)) |field| {
--- src/selection.zig
+++ src/selection.zig
@@ -1,1 +1,33 @@
+const std = @import("std");
 
+pub const operations = struct {
+    pub const pulse = struct {
+        pub const event = void;
+        pub const result = void;
+        pub fn call(payload: event, output: *align(16) [1024]u8) result {
+            _ = output;
+            _ = payload;
+        }
+    };
+    pub const print = struct {
+        pub const event = void;
+        pub const result = void;
+        pub fn call(payload: event, output: *align(16) [1024]u8) result {
+            _ = output;
+            _ = payload;
+        }
+    };
+    pub const add = struct {
+        pub const event = struct {
+            a: u32,
+            b: u32,
+        };
+        pub const result = u32;
+        pub fn call(payload: event, output: *align(16) [1024]u8) result {
+            _ = output;
+            const added = payload.a + payload.b;
+            std.debug.print("ran add {} + {} = {} \r\n", .{ payload.a, payload.b, added });
+            return added;
+        }
+    };
+};
